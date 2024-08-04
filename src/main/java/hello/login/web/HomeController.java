15bb512025bca3a2a7f56fd69ae54e6037a01cbb
junhui-LEE <junhee0386@naver.com>
2024-08-04 21:00:50 +0900

    로그인 처리하기 - 직접 만든 세션 적용

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,19 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
 //    @GetMapping("/")
     public String home() {
         return "home";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId,
                             Model model){
 //        쿠키를 받는 방법은 여러가지가 있다. HttpServletRequest에서 꺼낼수도 있고 여러가지 방법이 있지만 여기서는 스프링이 제공하는 @CookieValue를 쓰겠다.
@@ -40,6 +44,22 @@ public class HomeController {
 
         model.addAttribute("member", loginMember);
         return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request,
+                              Model model){
+
+        // 세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member)sessionManager.getSession(request);
+        if(member == null){
+            return "home";
+        }
+
+        // 로그인
+        model.addAttribute("member", member);
+        return "loginHome";
+
     }
 
 }
